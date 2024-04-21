a847d9bf281967144709edf9dc237d30efea06b1
asireddyvivekusf <asireddyv@usf.edu>
2024-04-21 04:45:47 -0400

    linked pages

--- js/main.js
+++ js/main.js
@@ -21,3 +21,15 @@ function closeFeedbackForm() {
     document.getElementById("feedback-overlay").style.display = "none";
     console.log('Feedback form closed');
 }
+
+var addItemId = 0;
+function addtoCart() {
+    //window.location.href = `index.html?data=${encodeURIComponent('hi')}`;
+    //console.log('done');
+    var item = document.getElementById('currentPackage');
+    localStorage.setItem('item', item);
+    localStorage.setItem('img', item.children[0].getAttribute('alt'));
+    localStorage.setItem('price', item.children[0].getAttribute('price'));
+    console.log(localStorage.getItem('img'));
+    window.location.href = "bookings.html";
+}
